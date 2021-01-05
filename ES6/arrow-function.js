1e36b2cff3d6fb370bb353756a0832d5ce279b84
Rat2 <leemagnon@naver.com>
2021-01-06 01:02:20 +0900

    DOM 객체를 document.querySelector를 이용하여 js파일에서 불러와 타입을 지정해주는 경우 타입 단언(as) 사용,

---
+++ ES6/arrow-function.js
@@ -0,0 +1,16 @@
+// ES5 - 함수 선언문
+function sum(a, b){
+    return a + b;
+}
+
+// ES5 - 함수 표현식
+var sum = function (a, b) {
+    return a + b;
+}
+
+// ES6 - 함수 표현식(화살표 함수)
+var sum1 = (a, b) => a + b;
+var sum2 = (a, b) => { return a + b; };
+
+// 타입스크립트의 화살표 함수
+var sum2 = (a: number, b: number): number => { return a + b; };
